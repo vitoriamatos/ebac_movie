0499df211560ccc94e6210f9d61fa55b093e7533
vitoriamatos <vitoria.matos99@gmail.com>
2023-04-10 14:57:12 -0300

    fix vercel bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin= require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
 function styles(){
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
@@ -13,8 +15,23 @@ function images(){
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = function() {
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
+
+// exports.default = function() {
     
-    gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(styles));
-    gulp.watch('./src/images/*', {ignoreInitial: false}, gulp.series(images));
+//     gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(styles));
+//     gulp.watch('./src/images/*', {ignoreInitial: false}, gulp.series(images));
+// }
+
+
+exports.default = gulp.parallel(styles, images, scripts);
+
+exports.watch = function() {
+
+    gulp.watch('./scr/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
